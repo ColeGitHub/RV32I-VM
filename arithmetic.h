f2490bbb0a3c242420fbf46c5fb42ac6fe948317
Cole <colehsmith38@gmail.com>
2019-03-29 14:43:02 -0400

    Renamed arithmetic, removed jump_utype header

--- arithmetic.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARITHMETIC_H
-#define ARITHMETIC_H
-
-#include "riscv.h"
-
-void arithmetic_handle(RISCVM* vm, uint32_t instr);
-
-#endif
--- jump_utype.h
+++
@@ -1,8 +0,0 @@
-#ifndef JUMP_UTYPE_H
-#define JUMP_UTYPE_H
-
-#include "riscv.h"
-
-void utype_handle(RISCVM* vm, uint32_t instr);
-
-#endif
