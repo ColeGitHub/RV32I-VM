c6a5b91fe4d2dc9d96135a80facfd1cfa5c4a4ed
Cole <colehsmith38@gmail.com>
2019-03-29 14:42:53 -0400

    Added test for lookup init

--- main.c
+++ main.c
@@ -13,17 +13,23 @@
 // BRANCH LOOKUP TEST
 int main()
 {
+    printf("Starting VM...\n");
+
     // Initialize Virtual Machine Memory
     RISCVM vm_struct = init_vm();  
     RISCVM* vm = &vm_struct;
 
     // Check for proper memory allocation, exit on error
+
     if (vm_struct.memory == NULL || vm_struct.registers == NULL) {
         printf("VM does not have enough memory.  Exiting...\n");
         return 1;
     } else {
         printf("Memory allocated.\n");
     }
+
+    init_opcode_lookup();
+
    
       
     // Free memory and exit
