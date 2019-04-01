a28e9c6e648cbcfe013151ce9bbdb35f4710cdc5
Cole <colehsmith38@gmail.com>
2019-04-01 16:17:58 -0400

    Define immediate size in header

--- arith.h
+++ arith.h
@@ -5,6 +5,8 @@
 
 #include "riscv.h"
 
+#define IMM_SIZE 12
+
 enum // arithmetic funct3 codes
 {
     ADD = 0,
@@ -17,19 +19,26 @@ enum // arithmetic funct3 codes
     AND = 7
 };
 
-int (*arith_lookup[8])(RISCVM*, uint32_t, uint8_t);
+typedef struct instr_args {
+    uint8_t rd;
+    uint8_t rs1;
+    uint8_t imm_flag;
+} instr_args;
+
+
+int (*arith_lookup[8])(RISCVM*, uint32_t, instr_args*);
 
 void init_arith_lookup(void);
 int arith_handle(RISCVM* vm, uint32_t instr);
 
-int add(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int sll(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int slt(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int sltu(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int _xor(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int sr(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int _or(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
-int _and(RISCVM* vm, uint32_t instr, uint8_t imm_flag);
+int add(RISCVM* vm, uint32_t instr, instr_args* args);
+int sll(RISCVM* vm, uint32_t instr, instr_args* args);
+int slt(RISCVM* vm, uint32_t instr, instr_args* args);
+int sltu(RISCVM* vm, uint32_t instr, instr_args* args);
+int _xor(RISCVM* vm, uint32_t instr, instr_args* args);
+int sr(RISCVM* vm, uint32_t instr, instr_args* args);
+int _or(RISCVM* vm, uint32_t instr, instr_args* args);
+int _and(RISCVM* vm, uint32_t instr, instr_args* args);
 
 int illegal_arith_funct3(RISCVM* vm, uint32_t instr);
 
